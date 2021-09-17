a6e774d6db8b10ebe5c8fd5c0c1bbb5e296cff61
Sumit Bhatia <SBhatia@rightpoint.com>
2021-09-17 13:56:51 +0530

    initial commit

---
+++ CustomFormElements/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CustomFormElements")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomFormElements")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b8b75622-8447-4a0c-8aa0-797728abdcd3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CustomFormElements/SaveAction/CustomSaveData.cs
@@ -0,0 +1,24 @@
+﻿using Sitecore.ExperienceForms.Models;
+using Sitecore.ExperienceForms.Processing;
+using Sitecore.ExperienceForms.Processing.Actions;
+
+namespace CustomFormElements.SaveAction
+{
+    public class CustomSaveData : SubmitActionBase<string>
+    {
+        public CustomSaveData(ISubmitActionData submitActionData) : base(submitActionData)
+        {
+        }
+
+        protected override bool Execute(string data, FormSubmitContext formSubmitContext)
+        {
+            return true;
+        }
+
+        protected override bool TryParse(string value, out string target)
+        {
+            target = string.Empty;
+            return true;
+        }
+    }
+}
